3d0d5ea95009ce4110daee923203abeb6bcbe64e
Mikhail Kolesnikov <dadyaaya@gmail.com>
2024-04-04 16:20:47 +0400

    added tailwind to the project

--- projects/vite-app/src/main.jsx
+++ projects/vite-app/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    The App 123
-  </React.StrictMode>,
-)
+const root = document.getElementById('root')
+
+ReactDOM
+    .createRoot(root)
+    .render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
