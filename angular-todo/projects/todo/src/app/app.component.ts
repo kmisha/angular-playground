d72b4526f6eeede49c2016d85618cbc6e168229c
Mikhail Kolesnikov <dadyaaya@gmail.com>
2024-08-05 16:18:23 +0400

    added Taiga UI library

--- angular-todo/projects/todo/src/app/app.component.ts
+++ angular-todo/projects/todo/src/app/app.component.ts
@@ -1,17 +1,37 @@
+import { TuiRootModule, TuiDialogModule, TuiAlertModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'mk-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+    selector: 'mk-root',
+    standalone: true,
+    imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+    template: `
+    <tui-root>
+        <!-- content of your app -->
 
-    <router-outlet />
+        <!--
+            If you need, you can add something between Taiga portal layers:
+            -->
+        <ng-container ngProjectAs="tuiOverContent">
+            <!-- Content over app content -->
+        </ng-container>
+        <ng-container ngProjectAs="tuiOverDialogs">
+            <!-- Content over dialogs -->
+        </ng-container>
+        <ng-container ngProjectAs="tuiOverAlerts">
+            <!-- Content over alerts -->
+        </ng-container>
+        <ng-container ngProjectAs="tuiOverPortals">
+            <!-- Content over dropdowns -->
+        </ng-container>
+        <ng-container ngProjectAs="tuiOverHints">
+            <!-- Content over hints -->
+        </ng-container>
+        </tui-root>
   `,
-  styles: [],
+    styles: [],
 })
 export class AppComponent {
-  title = 'todo';
+    title = 'todo';
 }
