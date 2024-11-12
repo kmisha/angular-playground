af822f3f82776bda23e36753ff48a0e5a7cfffca
Mikhail Kolesnikov <dadyaaya@gmail.com>
2024-11-12 13:43:51 +0400

    refactoring

--- libs/my-react-library/vite.config.ts
+++ libs/my-react-library/vite.config.ts
@@ -1,32 +1,28 @@
 import { defineConfig } from 'vite'
 import path from 'node:path';
+
+import react from "@vitejs/plugin-react";
 import dts from 'vite-plugin-dts';
-import react from '@vitejs/plugin-react'
+
+import { peerDependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         dts({
-            insertTypesEntry: true,
             rollupTypes: true,
-            include: ['src'],
-            tsconfigPath: './tsconfig.lib.json'
+            tsconfigPath: path.resolve(__dirname, 'tsconfig.lib.json'),
         })],
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'my-react-lib',
             fileName: 'my-react-lib',
-            formats: ['es', 'umd']
+            formats: ['es']
         },
         rollupOptions: {
-            external: ['react', 'react/jsx-runtime'],
-            output: {
-                globals: {
-                    react: 'React'
-                }
-            }
+            external: ['react/jsx-runtime', ...Object.keys(peerDependencies)]
         }
     }
 })
