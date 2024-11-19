aad2770ab03beb31093ed0155cd7f2174d9cfff1
Mikhail Kolesnikov <dadyaaya@gmail.com>
2024-11-19 16:03:46 +0400

    play with code

--- apps/calc-app/src/App.tsx
+++ apps/calc-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +7,15 @@ import {AtButton} from 'my-react-library';
 
 function App() {
     const [count, setCount] = useState(0)
+    const [log, setLog] = useState<readonly string[]>([]);
+
+    const increase = () => setCount(count => count + 1);
+    const decrease = () => setCount(count => count - 1);
+    const clearLog = () => setLog([]);
+
+    useEffect(() => {
+        setLog(log => [...log, `${count}`]);
+    }, [count])
 
     return (
         <>
@@ -20,18 +29,40 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                <button onClick={increase}>
+                    Increase
                 </button>
+
+                <button onClick={decrease}>
+                    Decrease
+                </button>
+
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    Count is {count}
                 </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            {log.length > 0 ? <Log log={log }/> : null}
+            <button onClick={clearLog}>Clear log</button>
             <AtButton label={'Test'}></AtButton>
         </>
+    )
+}
+
+export function Log({log}: {log: readonly string[]}): JSX.Element {
+    useEffect(() => {
+        console.log('mount Log');
+    }, []);
+
+    useEffect(() => {
+        console.log('unmount Log');
+    }, []);
+
+    useEffect(() => {
+        console.log('rerender');
+    });
+
+    return (
+        <pre>{log.join('\n')}</pre>
     )
 }
 
